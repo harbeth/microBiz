e828cb13fd01498c5a4cf06e3a26efecdbe4dcd1
ling deng <gloria.highefficiency@gmail.com>
2015-02-01 12:51:34 -0500

    inventory function

---
+++ microBiz/src/com/microBiz/controller/prd/InventoryAddActionController.java
@@ -0,0 +1,52 @@
+package com.microBiz.controller.prd;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.slim3.controller.Navigation;
+import org.slim3.util.BeanUtil;
+
+import com.microBiz.controller.BaseController;
+import com.microBiz.meta.ProductMeta;
+import com.microBiz.model.InventoryChange;
+import com.microBiz.model.PrdRatio;
+import com.microBiz.model.Product;
+import com.microBiz.service.ProductService;
+
+
+
+public class InventoryAddActionController extends BaseController{
+
+    private ProductService s;
+
+    public InventoryAddActionController(){
+        super();
+        s = new ProductService();
+
+
+    }
+    @Override
+    public Navigation run() throws Exception {
+
+        Product p = s.get(asKey("productKey"));
+        InventoryChange ic = new InventoryChange();
+        BeanUtil.copy(request,ic); 
+        ic.setChangeDate(new Date());
+        ic.setOriginalQty(p.getCurrentQty());
+        ic.setReasons("add inventory");
+        Double newQty = p.getCurrentQty() + ic.getChangeQty();
+        p.setCurrentQty(newQty);
+        ic.setNewQty(newQty);
+
+
+        s.save(p, ic);
+        List<Product> prds = s.getAll();
+        requestScope("prds", prds);
+        return forward("inventory-list.jsp");
+
+       
+    }
+    
+    
+}
---
+++ microBiz/src/com/microBiz/controller/prd/InventoryAddController.java
@@ -0,0 +1,35 @@
+package com.microBiz.controller.prd;
+import org.slim3.controller.Navigation;
+import org.slim3.util.BeanUtil;
+
+import com.google.appengine.api.datastore.Key;
+import com.microBiz.controller.BaseController;
+import com.microBiz.model.InventoryChange;
+import com.microBiz.model.Product;
+import com.microBiz.service.ProductService;
+
+
+
+public class InventoryAddController extends BaseController{
+    ProductService s = null;
+
+    public InventoryAddController(){
+        super();
+        s = new ProductService();
+    }
+    @Override
+    public Navigation run() throws Exception {
+
+        Product p = s.get(asKey("productKey"));
+        InventoryChange ic = new InventoryChange();
+     
+        BeanUtil.copy(ic, request);
+        requestScope("product", p);
+        return forward("inventory-add.jsp");
+
+       
+    }
+    
+    
+}
+
